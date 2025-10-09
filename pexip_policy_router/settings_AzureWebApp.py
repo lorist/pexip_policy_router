12bbddec60bb2a4ff977b73cf8049104cdce32cc
Jon Turner <Jon.Turner@hotmail.co.uk>
2025-10-09 11:37:10 +1100

    Update wsgi & new Azure settings file

---
+++ pexip_policy_router/settings_AzureWebApp.py
@@ -0,0 +1,11 @@
+from .settings import *
+import os
+
+# Configure the domain name using the environment variable
+# that Azure automatically creates for us.
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+
+# Other settings.py overides for production
+DEBUG = False
+SECRET_KEY = [os.environ['SECRET_KEY']]
+
