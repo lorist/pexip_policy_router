27f3a515d2af9afc7d34c50e3e2ade57df7a4098
Jon Turner <jon.turner@pexip.com>
2025-10-09 16:19:16 +1100

    Update settings_AzureWebApp.py

--- pexip_policy_router/settings_AzureWebApp.py
+++ pexip_policy_router/settings_AzureWebApp.py
@@ -5,6 +5,8 @@ import os
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
+
 # Other settings.py overides for production
 DEBUG = False
 SECRET_KEY = [os.environ['SECRET_KEY']]
